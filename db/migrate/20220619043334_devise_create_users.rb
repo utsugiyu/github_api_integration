b7cd8f62e1d93fe699e2d2989118d42a395b89aa
utsugiyu <utsugiu@gmail.com>
2022-06-19 14:59:57 +0900

    modified users table

--- db/migrate/20220619043334_devise_create_users.rb
+++ db/migrate/20220619043334_devise_create_users.rb
@@ -39,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps null: false
     end
 
-    add_index :users, :name,                unique: true
+    add_index :users, :name
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, [:uid, :provider], unique: true
